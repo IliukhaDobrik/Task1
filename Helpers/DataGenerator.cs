10a05bdaa24722ffc67ce9193ca7278a771824c8
IliukhaDobrik <bogdobrinskiy@gmail.com>
2023-12-05 19:58:42 +0300

    added combine files

--- Helpers/DataGenerator.cs
+++ Helpers/DataGenerator.cs
@@ -1,25 +1,25 @@
 ﻿using System.Text;
-using Bogus;
 
 namespace Helpers;
 
 public static class DataGenerator
 {
-    public static List<DateTime> DateGenerate(int count)
+    public static async Task<List<DateTime>> DateGenerate(int count)
     {
-        var faker = new Faker();
+        var rnd = new Random();
         var now = DateTime.UtcNow;
         List<DateTime> dates = new List<DateTime>();
 
         for (int i = 0; i < count; i++)
         {
-            dates.Add(faker.Date.Between(now, now.AddYears(-5)));
+            int days = rnd.Next(0, 365 * 5);
+            dates.Add(now.AddDays(-days));
         }
 
         return dates;
     }
 
-    public static List<string> StrGenerate(string language, int lenght, int count)
+    public static async Task<List<string>> StrGenerate(string language, int lenght, int count)
     {
         var rnd = new Random();
         const string ruChars = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
@@ -27,6 +27,7 @@ public static class DataGenerator
 
         List<string> result = new List<string>();
         var sb = new StringBuilder();
+        
         for (int i = 0; i < count; i++)
         {
             for (int j = 0; j < lenght; j++)
@@ -48,7 +49,7 @@ public static class DataGenerator
         return result;
     }
 
-    public static List<int> IntGenerate(int min, int max, int count)
+    public static async Task<List<int>> IntGenerate(int min, int max, int count)
     {
         var rnd = new Random();
         var result = Enumerable.Range(0, count).Select(x => rnd.Next(min, max+1)).ToList();
@@ -56,7 +57,7 @@ public static class DataGenerator
         return result;
     }
 
-    public static List<double> DoubleGenerate(int min, int max, int accuracy, int count)
+    public static async Task<List<double>> DoubleGenerate(int min, int max, int accuracy, int count)
     {
         var rnd = new Random();
         var result = new List<double>();
--- Task1/MainWindow.xaml.cs
+++ Task1/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using Helpers;
 
@@ -15,22 +20,66 @@ namespace Task1
         }
 
 
-        private void ButtonGenerate_OnClick(object sender, RoutedEventArgs e)
+        private async void ButtonGenerate_OnClick(object sender, RoutedEventArgs e)
         {
-            var dates = DataGenerator.DateGenerate(100);
-            var enStr = DataGenerator.StrGenerate("en", 10, 100);
-            var ruStr = DataGenerator.StrGenerate("ru", 10, 100);
-            var ints = DataGenerator.IntGenerate(1, 100_000_000, 100);
-            var doubles = DataGenerator.DoubleGenerate(1, 20, 8, 100);
+            const int lineCount = 100_000;
 
             for (int i = 0; i < 100; i++)
             {
+                var dates = await DataGenerator.DateGenerate(lineCount);
+                var enStr = await DataGenerator.StrGenerate("en", 10, lineCount);
+                var ruStr = await DataGenerator.StrGenerate("ru", 10, lineCount);
+                var ints = await DataGenerator.IntGenerate(1, 100_000_000, lineCount);
+                var doubles = await DataGenerator.DoubleGenerate(1, 20, 8, lineCount);
                 
-                // using (StreamWriter writer = new StreamWriter($"file{i}.txt"))
-                // {
-                //     
-                // }
+                using (var writer = new StreamWriter($"files/file{i}.txt"))
+                {
+                    for (int j = 0; j < lineCount; j++)
+                    {
+                        await writer.WriteLineAsync($"{dates[j].ToString("dd/MM/yyyy")}||{enStr[j]}||{ruStr[j]}||{ints[j]}||{doubles[j]}");
+                    }
+                }
             }
+
+            MessageBox.Show("Completed!");
+        }
+
+        private async void ButtonCombine_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int filesCount) == false || filesCount > 100 || filesCount < 2)
+            {
+                MessageBox.Show("Wrong count! Try again!");
+                return;
+            }
+
+            if (Directory.GetFiles("files").Length == 0)
+            {
+                MessageBox.Show("You dont have generated files!");
+                return;
+            }
+
+            var sb = new StringBuilder();
+            int countDropLines = 0;
+            for (int i = 0; i < filesCount; i++)
+            {
+                var allLines = await File.ReadAllLinesAsync($"files/file{i}.txt");
+                var linesWithoutSubstring = allLines.Where(x => x.Contains(textBox2.Text) == false).ToArray();
+                countDropLines += allLines.Length - linesWithoutSubstring.Length;
+
+                foreach (var line in linesWithoutSubstring)
+                {
+                    sb.Append(line + Environment.NewLine);
+                }
+            }
+            var combineText = sb.ToString();
+
+            var lenght = Directory.GetFiles("filesCombine").Length;
+            using (var writer = new StreamWriter($"filesCombine/filesCombine{lenght+1}.txt"))
+            {
+                await writer.WriteAsync(combineText);
+            }
+
+            MessageBox.Show($"Combined! Drop lines count: {countDropLines}");
         }
     }
 }
